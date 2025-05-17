2bb240e9b9fd55663812f7a5cb9ad8cfb1c90e2e
NicolasLerch <lerchnicolas@hotmail.com>
2025-05-17 15:37:21 -0300

    added mail validation

--- src/FormPage/FormPage.jsx
+++ src/FormPage/FormPage.jsx
@@ -1,21 +1,50 @@
 import "./FormPage.css";
-import whiteLogo from "../assets/supervielle-logo-white.png";
+// import whiteLogo from "../assets/supervielle-logo-white.png";
 import { supabase } from "../supaBaseClient";
 import { useState, useEffect } from "react";
 
 import Swal from "sweetalert2";
 
-import supervielleMineriaPurple from "../assets/sup-mineria-logo-purple.png";
-import supervielleMineriaWhite from "../assets/sup-mineria-logo-white.png";
-import supervielleMineriaRed from "../assets/sup-mineria-logo-red.png";
+// import supervielleMineriaPurple from "../assets/sup-mineria-logo-purple.png";
+// import supervielleMineriaWhite from "../assets/sup-mineria-logo-white.png";
+// import supervielleMineriaRed from "../assets/sup-mineria-logo-red.png";
 import supervielleMineria from "../assets/sup-mineria-logo.png";
 
 export default function FormPage() {
   const [prizesAvailables, setPrizesAvailables] = useState(true);
+
+  const mailExists = async (email) =>{
+    const { data: Clients, error } = await supabase.from("Clients").select("email").eq("email", email);
+    if (error) {
+      console.log(error);
+      console.log(Clients)
+    } else {
+      if (Clients.length > 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData);
+    data.email = data.email.trim().toLowerCase();
+
+    //Validaciones
+    const mail = formData.get("email").trim().toLowerCase();
+    const exists = await mailExists(mail);
+    if (exists) {
+      Swal.fire({
+        icon: "error",
+        title: "Correo ya registrado",
+        text: "El correo ya se encuentra registrado en nuestra base de datos.",
+        confirmButtonText: "Aceptar",
+      });
+      return;
+    }
 
     const phone = formData.get("phone");
     if (!/^\d{7,15}$/.test(phone)) {
@@ -36,10 +65,6 @@ export default function FormPage() {
         phone: data.phone,
       },
     ]);
-
-    if (error) {
-      console.error("Error al guardar:", error);
-    }
 
     if (error) {
       console.error("Error al guardar:", error);
@@ -94,15 +119,16 @@ export default function FormPage() {
         <div className="title-text-container">
           <p className="title-text">TE SIGUE EL RITMO</p>
         </div>
-        
       </div>
       <div className="form-container">
         <form className="home-form" id="home-form" onSubmit={handleSubmit}>
           <input type="text" placeholder="Nombre" name="name" required />
           <input type="text" placeholder="Apellido" name="last_name" required />
-          <input type="email" placeholder="Email" name="email" required/>
-          <input type="tel" placeholder="Celular" name="phone" required/>
-          <button className="submit-button" type="submit">Jugar</button>
+          <input type="email" placeholder="Email" name="email" required />
+          <input type="tel" placeholder="Celular" name="phone" required />
+          <button className="submit-button" type="submit">
+            Jugar
+          </button>
         </form>
       </div>
     </div>
